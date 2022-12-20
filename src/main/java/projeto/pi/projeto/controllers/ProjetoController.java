6445d15b00f6f4bfdf58e25d4423f79a85c886c3
Adrielysson <adrielysson10@gmail.com>
2022-12-20 09:52:53 -0300

    Adicionando primeiros arquivos do meu projeto

---
+++ src/main/java/projeto/pi/projeto/controllers/ProjetoController.java
@@ -0,0 +1,12 @@
+package projeto.pi.projeto.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProjetoController {
+	@RequestMapping("/")
+	public String Home() {
+		return "Home";
+	}
+}
---
+++ src/main/java/projeto/pi/projeto/models/Lojas.java
@@ -0,0 +1,5 @@
+package projeto.pi.projeto.models;
+
+public class Lojas {
+
+}
